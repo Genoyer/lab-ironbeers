4553251e80d38ee402ad4357c6c81e171a4f92e9
Luke Genoyer <lgenoyer@gmail.com>
2018-09-27 19:47:22 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -9,10 +8,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + "/views/patials");
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    let beersList = beers;
+    res.render('beers', {beersList});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
-app.listen(3000);
+app.get('/random-beers', (req, res, next) => {
+  punkApi.getRandom()
+  .then(beers => {
+
+  })
+  catch(error => {
+    console.log(error)
+  })
+});
+
+app.listen(3000, () => {
+  console.log("Live")
+});
